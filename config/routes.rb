317a5fa060a843a1a05a4e9bf41348eb08abaf59
lfborjas <luis.borjas@escolarea.com>
2011-09-11 21:04:07 -0600

    el controller de devise para omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 BucketList::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
 
   root :to => "home#index"
   # The priority is based upon order of creation:
